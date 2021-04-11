7f1be530263d3698b43cd4e6cee7ec8d83563300
dnatuna <dnatuna123@gmail.com>
2021-04-11 17:53:54 +0900

    Feat: 이미지 API 구현 및 Auth API 수정
    이미지를 업로드하고 볼 수 있는 API를 구현했습니다.
    더불어 Auth API들의 API 문서에 맞게 Response를 수정했습니다.

---
+++ src/services/ImageService.js
@@ -0,0 +1,17 @@
+import multer from 'multer';
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'images/');
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + file.originalname);
+  },
+});
+
+export const uploader = multer({ storage });
+
+export const upload = async (req, res, next) => {
+  const filePaths = req.files.map(file => file.path);
+  res.send(filePaths);
+};images/161813082941921taehee.png
